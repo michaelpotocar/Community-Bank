ce06c734a94be5711ceda9c963bbbc3e2b9fe97f
Michael Potocar <MichaelPotocar@gmail.com>
2023-03-09 16:51:15 -0500

    Successfully populated tables

--- src/persistDynamoDbData/lambda.js
+++ src/persistDynamoDbData/lambda.js
@@ -15,10 +15,18 @@ const { s3Client, ddbDocClient } = await import("./config.js").then((local) => {
 });
 
 const tableData = {
+    ProjectKittyCustomers: {
+        Bucket: "projectkitty",
+        Key: "ProjectKittyCustomers.json"
+    },
+    ProjectKittyAccounts: {
+        Bucket: "projectkitty",
+        Key: "ProjectKittyAccounts.json"
+    },
     ProjectKittyTransactions: {
         Bucket: "projectkitty",
         Key: "ProjectKittyTransactions.json"
-    }
+    },
 };
 
 export const handler = async function (event, context) {
@@ -40,6 +48,50 @@ export const handler = async function (event, context) {
         const batchSize = 25;
         const batches = [];
 
+        if (table == 'ProjectKittyCustomers') {
+            for (let i = 0; i < entries.length / batchSize; i++) {
+                const filtered = entries.filter(({ }, index) => index >= batchSize * i && index < batchSize * (i + 1));
+                const mapped = filtered.map(entry => {
+                    const mappedFriends = entry.friends.map(friend => {
+                        return {
+                            "N": String(friend),
+                        };
+                    });
+                    return {
+                        "PutRequest": {
+                            "Item": {
+                                id: { N: String(entry.id) },
+                                firstName: { S: String(entry.firstName) },
+                                lastName: { S: String(entry.lastName) },
+                                friends: { L: mappedFriends },
+                            }
+                        }
+                    };
+                });
+                batches.push(mapped);
+            }
+        }
+
+        if (table == 'ProjectKittyAccounts') {
+            for (let i = 0; i < entries.length / batchSize; i++) {
+                const filtered = entries.filter(({ }, index) => index >= batchSize * i && index < batchSize * (i + 1));
+                const mapped = filtered.map(entry => {
+                    return {
+                        "PutRequest": {
+                            "Item": {
+                                accountNumber: { N: String(entry.accountNumber) },
+                                routingNumber: { N: String(entry.routingNumber) },
+                                customerId: { N: String(entry.customerId) },
+                                nickname: { S: String(entry.nickname) },
+                                type: { S: String(entry.type) },
+                            }
+                        }
+                    };
+                });
+                batches.push(mapped);
+            }
+        }
+
         if (table == 'ProjectKittyTransactions') {
             for (let i = 0; i < entries.length / batchSize; i++) {
                 const filtered = entries.filter(({ }, index) => index >= batchSize * i && index < batchSize * (i + 1));
@@ -51,7 +103,7 @@ export const handler = async function (event, context) {
                                 submittedDateTime: { N: String(entry.submittedDateTime) },
                                 completedDateTime: { N: String(entry.completedDateTime) },
                                 amount: { N: String(entry.amount) },
-                                memo: { S: String(entry.memo) }
+                                memo: { S: String(entry.memo) },
                             }
                         }
                     };
